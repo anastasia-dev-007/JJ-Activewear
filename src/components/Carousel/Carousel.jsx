936522edcc4a358c2d3a412e2cfb80757eb888a9
anastasia-dev-007 <133599958+anastasia-dev-007@users.noreply.github.com>
2023-11-13 15:03:34 +0200

    Imported carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,67 +1,34 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import lp1 from "../../components/assets/lp1.jpg";
 import lp2 from "../../components/assets/lp2.jpg";
 import lp3 from "../../components/assets/lp3.jpg";
 import logo1wh from "../../components/assets/logo1wh.png";
-import styles from './Carousel.module.css'
+import styles from './Carousel.module.css';
 
-const Carousel = () => {
-  const images = [
-    {
-      id: 1,
-      img: lp1,
-    },
-    {
-      id: 2,
-      img: lp2,
-    },
-    {
-      id: 3,
-      img: lp3,
-    },
-  ];
+const MyCarousel = () => {
+  const images = [lp1, lp2, lp3];
 
   return (
-    <div>
-      <div className={styles.carouselContainer}>
-
-        <div className={styles.carouselWindow}>
-          {images.map((image, index) => (
-            <img key={index} src={image.img} alt='' />
-          ))}
-        </div>
-
-        <div className={styles.onCarouselWindow}>
-          <div className={styles.onCarouselElements}>
-            <i class="fa-solid fa-chevron-left" ></i>
-
-            <div className={styles.landingText}>
-              <header>
-                <img src={logo1wh} alt="logo3" width='100px' />
-                <h1>ACTIVEWEAR</h1>
-              </header>
-
-              <p className={styles.question}>DO YOU SERACH FOR HIGH-QUALITY SCULPTING ACTIVEWEAR?</p>
-              <p>Discover the ultimate workout experience with JJ Activewear premium activewear collection. Our trendy and functional designs are made with high-quality materials to help you achive your fitness goals in style.</p>
-              <a href='#'>SHOP NOW</a>
-            </div>
-
-            <i class="fa-solid fa-chevron-right"></i>
-          </div>
-
-
-          <div className={styles.carouselBullets}>
-            <i class="fa-solid fa-circle"></i>
-            <i class="fa-regular fa-circle"></i>
-            <i class="fa-regular fa-circle"></i>
-          </div>
-        </div>
-
-
-      </div>
-
-    </div>
+    <Carousel>
+      {images.map((image, index) => (
+        <Carousel.Item key={index}>
+          <img className="d-block w-100" src={image} alt={`Slide ${index + 1}`} />
+          <Carousel.Caption className={styles.onCarouselElements}>
+            <img src={logo1wh} alt="logo3" width='100px' />
+            <h1>ACTIVEWEAR</h1>
+            <p className={styles.question}>DO YOU SEARCH FOR HIGH-QUALITY SCULPTING ACTIVEWEAR?</p>
+            <p>
+              Discover the ultimate workout experience with JJ Activewear's premium activewear collection.
+              Our trendy and functional designs are made with high-quality materials to help you achieve your fitness goals in style.
+            </p>
+            <a href='#'>SHOP NOW</a>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
+    </Carousel>
   );
 };
 
-export default Carousel;
+export default MyCarousel;
