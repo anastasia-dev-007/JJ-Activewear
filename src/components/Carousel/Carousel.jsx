3671bf4a9e348306cdd80333383c3d8b65d25b41
anastasia-dev-007 <133599958+anastasia-dev-007@users.noreply.github.com>
2023-11-13 15:22:06 +0200

    carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -5,29 +5,31 @@ import lp1 from "../../components/assets/lp1.jpg";
 import lp2 from "../../components/assets/lp2.jpg";
 import lp3 from "../../components/assets/lp3.jpg";
 import logo1wh from "../../components/assets/logo1wh.png";
-import styles from './Carousel.module.css';
+import styles from './Carousel.module.css'; // Assuming your CSS file is named Carousel.module.css
 
 const MyCarousel = () => {
   const images = [lp1, lp2, lp3];
 
   return (
-    <Carousel>
-      {images.map((image, index) => (
-        <Carousel.Item key={index}>
-          <img className="d-block w-100" src={image} alt={`Slide ${index + 1}`} />
-          <Carousel.Caption className={styles.onCarouselElements}>
-            <img src={logo1wh} alt="logo3" width='100px' />
-            <h1>ACTIVEWEAR</h1>
-            <p className={styles.question}>DO YOU SEARCH FOR HIGH-QUALITY SCULPTING ACTIVEWEAR?</p>
-            <p>
-              Discover the ultimate workout experience with JJ Activewear's premium activewear collection.
-              Our trendy and functional designs are made with high-quality materials to help you achieve your fitness goals in style.
-            </p>
-            <a href='#'>SHOP NOW</a>
-          </Carousel.Caption>
-        </Carousel.Item>
-      ))}
-    </Carousel>
+    <div className={styles.carouselContainer}>
+      <Carousel className={styles.carouselWindow}>
+        {images.map((image, index) => (
+          <Carousel.Item key={index}>
+            <img className="d-block w-100" src={image} alt={`Slide ${index + 1}`} />
+            <Carousel.Caption className={`${styles.onCarouselElements} ${styles.onCarouselWindow}`}>
+              <img src={logo1wh} alt="logo3" width='100px' />
+              <h1>ACTIVEWEAR</h1>
+              <p className={styles.question}>DO YOU SEARCH FOR HIGH-QUALITY SCULPTING ACTIVEWEAR?</p>
+              <p>
+                Discover the ultimate workout experience with JJ Activewear's premium activewear collection.
+                Our trendy and functional designs are made with high-quality materials to help you achieve your fitness goals in style.
+              </p>
+              <a href='#'>SHOP NOW</a>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
   );
 };
 
