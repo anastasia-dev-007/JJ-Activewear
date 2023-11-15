b947ad78fa7ebd1d3194ea117c81b779c10c3a55
anastasia-dev-007 <133599958+anastasia-dev-007@users.noreply.github.com>
2023-11-15 18:22:50 +0200

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import LandingPage from './routes/LandingPage/LandingPage';
+import ProductListing from './routes/ProductListing/ProductListing';
+import ProductDetails from './routes/ProductDetails/ProductDetails';
+import ShoppingCart from './routes/ShoppingCart/ShoppingCart';
+import Checkout from './routes/Checkout/Checkout';
+import OrderConfirmation from './routes/OrderConfirmation/OrderConfirmation';
+import Login from './routes/Login/Login';
 
 
 function App() {
@@ -11,9 +17,16 @@ function App() {
       <NavBar />
 
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<LandingPage/>}/>
-      </Routes>
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/product-list' element={<ProductListing />} />
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/product-details' element={<ProductDetails />} />
+          <Route path='/shopping-cart' element={<ShoppingCart />} />
+          <Route path='/checkout' element={<Checkout />} />
+          <Route path='/order-confirmation' element={<OrderConfirmation />} />
+          <Route path='*' element={<>Page not found</>}></Route>
+        </Routes>
       </BrowserRouter>
 
       <Footer />
