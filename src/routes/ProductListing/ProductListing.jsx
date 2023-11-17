8d88a3e6c4f8a02b761d5d49acdfa2a7043c445d
anastasia-dev-007 <133599958+anastasia-dev-007@users.noreply.github.com>
2023-11-17 11:42:25 +0200

    Developed 1st accordion

--- src/routes/ProductListing/ProductListing.jsx
+++ src/routes/ProductListing/ProductListing.jsx
@@ -4,6 +4,7 @@ import styles from "../ProductListing/ProductListing.module.css"
 
 const ProductListing = () => {
   const [products, setProducts] = useState([]);
+  const [openAccordions, setOpenAccordions] = useState([]); //openAccordions is an array that keeps track of the accordion items that are currently open.
 
   useEffect(() => {
     // Used the `getProducts` function to fetch products data and update the state.
@@ -11,6 +12,43 @@ const ProductListing = () => {
 
     setProducts(data);
   }, []);
+
+  const categoryAccordionData = [
+    {
+      id: 1,
+      title: 'Activewear',
+      list: [
+        { id: 1, title: 'Tops & Sport Bars' },
+        { id: 2, title: 'T-shirts' },
+        { id: 3, title: 'Long-sleeve workout tops' },
+        { id: 4, title: 'Tennis Shorts' },
+        { id: 5, title: 'Leggings & Yoga Pants' },
+        { id: 6, title: 'Matching Sets' }]
+    },
+    {
+      id: 2,
+      title: 'Accessories',
+      list: [
+        { id: 1, title: 'Sport Bags' },
+        { id: 2, title: 'Corsets' },
+        { id: 3, title: 'Running Sock' }
+      ]
+    },
+    {
+      id: 3,
+      title: 'Swimwear',
+      list: [{ id: 1, title: 'Swimwear' },]
+    }
+  ];
+
+  //toggleAccordion function is responsible for managing which accordions are open and which ones are closed.
+  const toggleAccordion = (id) => {
+    if (openAccordions.includes(id)) { //  // Check if the accordion with this id is already in the openAccordions array
+      setOpenAccordions(openAccordions.filter(acc => acc !== id));  // If it is already open, close it by updating openAccordions to exclude the current id. If the accordion is already open ('includes' returns 'true'), it removes that id from the openAccordions array. It uses the 'filter' function to create a new array that includes all the items from openAccordions except the one with the id clicked on.
+    } else {
+      setOpenAccordions([...openAccordions, id]); //If the accordion is closed ('includes' returns 'false'), means it's opening. So, it sets the openAccordions array to a new array that contains all the existing items from openAccordions ([...openAccordions]) and adds the new id to it.
+    }
+  };
 
   return (
     <div>
@@ -28,7 +66,23 @@ const ProductListing = () => {
 
       {/* to copy style from Aimo */}
       <div className='filterContainer'>
-        <div className='categoryAccordions'></div>
+        <div className='categoryAccordion'>
+          {categoryAccordionData.map(item => (
+            <div key={item.id} className="accordionItem">
+              <div className="accordionTitle" onClick={() => toggleAccordion(item.id)}>
+                <div>{item.title}</div>
+                <div>{openAccordions.includes(item.id) ? '-' : '+'}</div> {/*displays +/-  based on whether the current accordion is open (i.e., its id is in the openAccordions array). */}
+              </div>
+              {openAccordions.includes(item.id) && ( //checks if the current accordion is open. If open, accordion content is rendered.
+                <div className="accordionList">
+                  {item.list.map(listItem => (
+                    <div key={listItem.id}>{listItem.title}</div>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
         <div className='colorsAccordions'></div>
         <div className='sizeAcordeon'></div>
         <div className='priceRuler'></div>
