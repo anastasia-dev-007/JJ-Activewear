5031bd443ff09142e1755e23bbbea6911b972210
anastasia-dev-007 <133599958+anastasia-dev-007@users.noreply.github.com>
2023-12-02 17:10:19 +0200

    How to saveProducts?

--- src/routes/AdminPanel/AdminPanel.jsx
+++ src/routes/AdminPanel/AdminPanel.jsx
@@ -3,9 +3,37 @@ import Tabs from 'react-bootstrap/Tabs';
 import styles from './AdminPanel.module.css'
 import { products } from '../../products.service';
 import Form from 'react-bootstrap/Form';//for Modal
-
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import { useRef, useState } from 'react';
+import { saveProduct } from '../../products.service';
 
 function AdminPanel() {
+    const [newProduct, setNewProduct] = useState({
+        id: '',
+        imgs: [],
+        title: '',
+        titleCode: '',
+        color: '',
+        category: '',
+        subcategory: '',
+        subcategoryCode: '',
+        size: '',
+        quantity: '',
+        bestSellerStatus: '',
+        currency: '',
+        price: '',
+        promo: '',
+        promoPrice: '',
+        productDescription: '',
+    });
+
+    const fileInputRef = useRef(null);
+
+const handleFileChange = (e) => {
+    const files = Array.from(e.target.files);
+    setNewProduct({ ...newProduct, imgs: files });
+};
+
     return (
         <div className={styles.adminPanelContainer}>
             <Tabs
@@ -73,18 +101,97 @@ function AdminPanel() {
                 <Tab eventKey="addProducts" title="Add New Products">
                     <div>
                         <span>Product ID</span>
-                        <input type="text" disabled/>
+                        <input type="text" disabled />
                     </div>
-                    
+
                     <Form.Group controlId="formFile" className="mb-3">
-                                            <Form.Label>Upload your photos</Form.Label>
-                                            <Form.Control type="file" />
-                                            <Form.Control type="file" />
-                                            <Form.Control type="file" />
-                                            <Form.Control type="file" />
-                                            <Form.Control type="file" />
-                                        </Form.Group>
-                                        <input type="text" name="" id="" />
+                        <Form.Label>Upload your photos</Form.Label>
+                        <Form.Control type="file" ref={fileInputRef} onChange={handleFileChange} multiple />
+                        <Form.Control type="file" ref={fileInputRef} onChange={handleFileChange} multiple/>
+                        <Form.Control type="file" ref={fileInputRef} onChange={handleFileChange} multiple/>
+                        <Form.Control type="file" ref={fileInputRef} onChange={handleFileChange} multiple/>
+                        <Form.Control type="file" ref={fileInputRef} onChange={handleFileChange} multiple/>
+                    </Form.Group>
+
+                    <FloatingLabel
+                        controlId="floatingTitle"
+                        label="Product Title"
+                        className="mb-3">
+                        <Form.Control type="text" placeholder="Enter product title..."
+                            value={newProduct.title}
+                            onChange={(e) => setNewProduct({ ...newProduct, title: e.target.value })} />
+                    </FloatingLabel>
+
+                    <FloatingLabel controlId="floatingTitleCode" label="Title Code">
+                        <Form.Control type="text" placeholder="Enter product title code..."
+                            value={newProduct.titleCode}
+                            onChange={(e) => setNewProduct({ ...newProduct, titleCode: e.target.value })} />
+                    </FloatingLabel>
+
+                    <FloatingLabel controlId="floatingColor" label="Color">
+                        <Form.Control type="text" placeholder="Enter product color..."
+                            value={newProduct.color}
+                            onChange={(e) => setNewProduct({ ...newProduct, color: e.target.value })} />
+                    </FloatingLabel>
+
+
+                    <FloatingLabel controlId="floatingCategory" label="Category">
+                        <Form.Control type="text" placeholder="" value={newProduct.category}
+                            onChange={(e) => setNewProduct({ ...newProduct, category: e.target.value })} />
+                    </FloatingLabel>
+
+
+                    <FloatingLabel controlId="floatingSubcategory" label="Subcategory">
+                        <Form.Control type="text" placeholder="" value={newProduct.subcategory}
+                            onChange={(e) => setNewProduct({ ...newProduct, subcategory: e.target.value })} />
+                    </FloatingLabel>
+
+                    <FloatingLabel controlId="floatingSubcategoryCode" label="Subcategory Code">
+                        <Form.Control type="text" placeholder="" value={newProduct.subcategoryCode}
+                            onChange={(e) => setNewProduct({ ...newProduct, subcategoryCode: e.target.value })} />
+                    </FloatingLabel>
+
+                    <FloatingLabel controlId="floatingSize" label="Size">
+                        <Form.Control type="text" placeholder="" value={newProduct.size}
+                            onChange={(e) => setNewProduct({ ...newProduct, size: e.target.value })} />
+                    </FloatingLabel>
+
+                    <FloatingLabel controlId="floatingQuantity" label="Quantity">
+                        <Form.Control type="text" placeholder="" value={newProduct.quantity}
+                            onChange={(e) => setNewProduct({ ...newProduct, quantity: e.target.value })} />
+                    </FloatingLabel>
+
+                    <FloatingLabel controlId="floatingBestSellerStatus" label="Best Seller Status">
+                        <Form.Control type="text" placeholder="" value={newProduct.bestSellerStatus}
+                            onChange={(e) => setNewProduct({ ...newProduct, bestSellerStatus: e.target.value })} />
+                    </FloatingLabel>
+
+                    <FloatingLabel controlId="floatingCurrency" label="Currency">
+                        <Form.Control type="text" placeholder="" value={newProduct.currency}
+                            onChange={(e) => setNewProduct({ ...newProduct, currency: e.target.value })} />
+                    </FloatingLabel>
+
+                    <FloatingLabel controlId="floatingPrice" label="Price">
+                        <Form.Control type="number" placeholder="" value={newProduct.price}
+                            onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })} />
+                    </FloatingLabel>
+
+                    <FloatingLabel controlId="floatingPromo" label="Promo">
+                        <Form.Control type="text" placeholder="" value={newProduct.promo}
+                            onChange={(e) => setNewProduct({ ...newProduct, promo: e.target.value })} />
+                    </FloatingLabel>
+
+                    <FloatingLabel controlId="floatingPromoPrice" label="Promo Price">
+                        <Form.Control type="text" placeholder="" value={newProduct.promoPrice}
+                            onChange={(e) => setNewProduct({ ...newProduct, promoPrice: e.target.value })} />
+                    </FloatingLabel>
+
+                    <FloatingLabel controlId="floatingDescription" label="Description">
+                        <Form.Control type="textare" placeholder="" value={newProduct.productDescription}
+                            onChange={(e) => setNewProduct({ ...newProduct, productDescription: e.target.value })} />
+                    </FloatingLabel>
+
+                    <button onClick={() => saveProduct(newProduct)}>SAVE PRODUCT</button>
                 </Tab>
                 <Tab eventKey="orders" title="Orders">
                     <table class="table">
