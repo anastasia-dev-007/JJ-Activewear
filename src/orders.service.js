77d1f4dd2edc7cb6d093215ef43d152ed3ca2a9c
anastasia-dev-007 <133599958+anastasia-dev-007@users.noreply.github.com>
2023-12-23 12:29:05 +0200

    commit

--- src/orders.service.js
+++ src/orders.service.js
@@ -1,36 +1,35 @@
 export const orders = [];
 
-const currentDate = new Date();
-
-const order = {
-    id: Date.now(),
-    name: '', //admin or user
-    email: '',
-    country: '',
-    address: '',
-    phoneNumber: '',
-    cartItems: [
-        {
-            id: '',
-            imgs: [],
-            title: '',
-            titleCode: '',
-            color: '',
-            category: '',
-            subcategory: '',
-            subcategoryCode: '',
-            size: { S: '', M: '', L: '', },
-            bestSellerStatus: '',
-            newArrival: '',
-            currency: '',
-            price: '',
-            promo: '',
-            promoPrice: '',
-            productDescription: '',
-        }
-    ],
-    orderDate: currentDate.toISOString(),
-};
+// const currentDate = new Date();
+// const order = {
+//     id: Date.now(),
+//     name: '', //admin or user
+//     email: '',
+//     country: '',
+//     address: '',
+//     phoneNumber: '',
+//     cartItems: [
+//         {
+//             id: '',
+//             imgs: [],
+//             title: '',
+//             titleCode: '',
+//             color: '',
+//             category: '',
+//             subcategory: '',
+//             subcategoryCode: '',
+//             size: { S: '', M: '', L: '', },
+//             bestSellerStatus: '',
+//             newArrival: '',
+//             currency: '',
+//             price: '',
+//             promo: '',
+//             promoPrice: '',
+//             productDescription: '',
+//         }
+//     ],
+//     orderDate: currentDate.toISOString(),
+// };
 
 export const saveOrder = (order) => {
     orders.push(order);
